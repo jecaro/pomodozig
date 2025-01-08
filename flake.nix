7e469600fd6b2ebd14d986522ac70f830787fab1
jecaro <jeancharles.quillet@gmail.com>
2025-01-08 21:14:44 +0100

    Add the nix derivation

--- flake.nix
+++ flake.nix
@@ -3,14 +3,40 @@
   outputs = { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.default ];
+      });
     in
     {
-      devShell.x86_64-linux =
-        pkgs.mkShell {
-          buildInputs = [
-            pkgs.zig
-            pkgs.zls
-          ];
+      overlays.default = (final: prev: {
+        pomodozig = prev.stdenv.mkDerivation {
+          name = "pomodozig";
+          src = self;
+          nativeBuildInputs = [ pkgs.zig.hook ];
         };
+      });
+
+      packages = forAllSystems (system: {
+        pomodozig = nixpkgsFor.${system}.pomodozig;
+        default = self.packages.${system}.pomodozig;
+      });
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.zig
+              pkgs.zls
+            ];
+          };
+        }
+      );
     };
 }
